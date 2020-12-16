df8199b099a16404fd6b774b9a0e097b3e3c5f1a
Mateus Arthur da Silva de Freitas <65245536+masFreitas@users.noreply.github.com>
2020-12-16 20:33:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SomaDoisNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Algoritmos 2 - Leia dois numeros inteiros e escreva a soma deles
+            int n1, n2, soma;
+
+            Console.WriteLine("Escreva o primeiro numero");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escreva o segundo numero");
+            n2 = int.Parse(Console.ReadLine());
+
+            soma = n1 + n2;
+
+            Console.WriteLine("A soma de {0} e o {1} tem como resultado {2}", n1, n2, soma);
+        }
+    }
+}
